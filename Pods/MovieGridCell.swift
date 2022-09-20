b30f365b3135896fe3ab2ac66d589ab9b8f54640
sharon-zheng <sharonzheng0.0@gmail.com>
2022-09-20 02:55:47 -0400

    update flix part2

---
+++ Pods/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by sharon on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
